c71c16c57a43467ba52cabaf4114989390714ed2
PedroVMota <pvital-m@student.42lisboa.com>
2024-06-08 22:59:12 +0100

    Docker:

---
+++ backend/Server/backend/middleware.py
@@ -0,0 +1,23 @@
+# backend/middleware.py
+from django.utils.deprecation import MiddlewareMixin
+from Auth.models import serverLogs
+
+
+class LoggingMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        print(">>>>>>> LOG SYSTEM TO THE TABLE <<<<<<")
+        if request.user.is_authenticated:
+            serverLogs.objects.create(
+                user=request.user,
+                mehod=request.method,
+                path=request.path,
+                status=response.status_code
+            )
+        else:
+            serverLogs.objects.create(
+                user=None,
+                mehod=request.method,
+                path=request.path,
+                status=response.status_code
+            )
+        return response
