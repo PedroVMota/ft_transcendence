18ad9e6f2020c460fdd2a49f209d27ddf53a43ea
PedroVMota <pvital-m@student.42lisboa.com>
2024-06-10 19:04:48 +0100

    WebSocket

--- backend/Server/Sockets/consumers.py
+++ backend/Server/Sockets/consumers.py
@@ -1,40 +1,62 @@
 # Sockets/consumers.py
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 
-class Menssage():
-    def __init__(self, msg) -> None:
-        self._HANDSHAKE = 'handshake' + msg
-        self._MESSAGE = 'message' + msg
-        self._ERROR = 'error' + msg
-        self._SUCCESS = 'success' + msg
-        self._CLOSE = 'close' + msg
-        self._data = None
+class GeneralChat(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'general'
+        self.user = self.scope["user"]
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': f'{self.user.username} has joined the chat.'
+            }
+        )
+
+    async def disconnect(self, close_code):
+        # Leave room group
         
+        await self.channel_layer.send({
+            'type': 'chat_message',
+            'message': f'{self.user.username} has left the chat.'
+        })
 
-class YourConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-        print(Menssage('Connected! ')._HANDSHAKE)
-        
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def disconnect(self, close_code):
-        pass
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-    def receive(self, text_data):
-        try:
-            text_data_json = json.loads(text_data)
-            print(text_data_json)
-            message = text_data_json['message']
-            print(Menssage(message)._MESSAGE)
-            self.send(text_data=json.dumps({
-                'user': 'Server',
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
                 'message': message
-            }))
-        except Exception as e:
-            print(Menssage(str(e))._ERROR)
-            self.send(text_data=json.dumps({
-                'Error': str(e)
-            }))
-        
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': self.user.username + ': ' + message
+        }))
--- backend/Server/Sockets/routing.py
+++ backend/Server/Sockets/routing.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/', consumers.YourConsumer.as_asgi()),
+    # path('ws/', consumers.YourConsumer.as_asgi()),
+    path('ws/general/', consumers.GeneralChat.as_asgi())
 ]
 
 
