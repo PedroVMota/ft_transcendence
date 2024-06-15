daa6b6ac033221fe738c198a3623f24b0d5360cb
PedroVMota <pvital-m@student.42lisboa.com>
2024-06-15 17:36:43 +0100

    Origin

--- backend/Server/backend/settings.py
+++ backend/Server/backend/settings.py
@@ -42,6 +42,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CORS_ALLOW_CREDENTIALS = True
