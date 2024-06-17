612c03ed7cf20257ea56a8b29c8203674206e46f
PedroVMota <pvital-m@student.42lisboa.com>
2024-06-17 16:43:20 +0100

    Menu

--- frontend/Code/Assets/Js/Components/App.js
+++ frontend/Code/Assets/Js/Components/App.js
@@ -1,6 +1,7 @@
 import { showAlert } from "./Alert";
 import { login } from "./Login";
 import { treeJSAplication } from "../treeJSAplication";
+import { Menu } from "./Menu/Menu";
 
 /*
     * Function to render the login page
@@ -11,34 +12,41 @@ export function app() {
         _AppLogin.remove();
     }
 
-    if (!document.getElementById('logoutButton')) {
-        var logoutButton = document.createElement('button');
-        logoutButton.id = 'logoutButton';
-        logoutButton.textContent = 'Logout';
-        logoutButton.addEventListener('click', function () {
-            window.history.pushState({}, '', '/');
-            document.body.innerHTML = '';
-            localStorage.removeItem('Access');
-            localStorage.removeItem('Refresh');
-            login();
-        });
+    var _App = document.createElement('div');
+    _App.id = '_App';
+    _App.classList.add('bg-primary');
+    document.body.appendChild(_App);
+    
+    Menu(_App);
 
-        document.body.appendChild(logoutButton);
-    }
+    // if (!document.getElementById('logoutButton')) {
+    //     var logoutButton = document.createElement('button');
+    //     logoutButton.id = 'logoutButton';
+    //     logoutButton.textContent = 'Logout';
+    //     logoutButton.addEventListener('click', function () {
+    //         window.history.pushState({}, '', '/');
+    //         document.body.innerHTML = '';
+    //         localStorage.removeItem('Access');
+    //         localStorage.removeItem('Refresh');
+    //         login();
+    //     });
 
-    var heading = document.createElement('h1');
-    heading.textContent = 'Welcome to the app!';
-    document.body.appendChild(heading);
-
-    var paragraph = document.createElement('p');
-    paragraph.textContent = 'This is some more content on the page.';
-    document.body.appendChild(paragraph);
-
+        // _App.body.appendChild(logoutButton);
+    // }
+// 
+    // var heading = document.createElement('h1');
+    // heading.textContent = 'Welcome to the app!';
+    // document.body.appendChild(heading);
+// 
+    // var paragraph = document.createElement('p');
+    // paragraph.textContent = 'This is some more content on the page.';
+    // document.body.appendChild(paragraph);
+// 
     // Ensure the canvas is added after the paragraph
-    var canvasContainer = document.createElement('div');
-    canvasContainer.id = 'canvasContainer';
-    document.body.appendChild(canvasContainer);
-
+    // var canvasContainer = document.createElement('div');
+    // canvasContainer.id = 'canvasContainer';
+    // document.body.appendChild(canvasContainer);
+// 
     // Modify treeJSAplication to accept a container element for the renderer
-    treeJSAplication(canvasContainer);
+    // treeJSAplication(canvasContainer);
 }
